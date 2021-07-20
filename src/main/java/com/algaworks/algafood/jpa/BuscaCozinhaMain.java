8dc2e42d078929670f903dbcbb0e7bd3b73876f7
Rodrigo <rodrigoassis1997@gmail.com>
2021-07-20 20:16:04 -0300

    Atualizando um objeto no banco de dados

--- src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
@@ -1,6 +1,5 @@
 package com.algaworks.algafood.jpa;
 
-import java.util.List;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class BuscaCozinhaMain {
 
 	public static void main(String[] args) {
 		/*
@@ -21,24 +20,10 @@ public class InclusaoCozinhaMain {
 		
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 		
+		Cozinha cozinha = new Cozinha();
+		cozinha = cadastroCozinha.buscar(2L);
 		
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brasileira");
-		
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setId(1L);
-		cozinha2.setNome("Japonesa");
-		
-		cadastroCozinha.salvar(cozinha1);
-		cadastroCozinha.salvar(cozinha2);
-		
-		
-		List<Cozinha> cozinhas = cadastroCozinha.listar();
-		
-		for(Cozinha cozinha : cozinhas) {
-			System.out.println(cozinha.getNome());
-		}
-		
-		
+		System.out.println(cozinha.getId());
+		System.out.println(cozinha.getNome());
 	}
 }
--- src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -29,14 +29,25 @@ public class CadastroCozinha {
 		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
 	}
 	
+	/*
+	 * Realiza a busca por id
+	 */
+	@Transactional
+	public Cozinha buscar(Long id) {
+		return manager.find(Cozinha.class, id);
+	}
+	
 	/**
 	 * Persiste as informações do objeto no banco de dados e retorna o objeto com as informações persistida (se tiver
 	 * um id gerado, essa informação é retornada também)
+	 * 
+	 * Caso o objeto passado como parâmetro já tiver um id salvo no banco, então esse objeto terá suas informações 
+	 * atualizadas no banco, conforme o id.
 	 * @param cozinha
 	 * @return
 	 */
 	@Transactional
-	public Cozinha adicionar(Cozinha cozinha) {
+	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
 }
