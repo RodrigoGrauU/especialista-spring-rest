5365602e467d64ce7133c1e81b7187b8bd238aa0
Rodrigo <rodrigoassis1997@gmail.com>
2021-07-22 09:42:35 -0300

    inserção do Lombok e repositório de restaurantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into cozinha(nome) values('Tailandesa');
 insert into cozinha(nome) values('Portuguesa');
+
+insert into restaurante(nome, taxa_frete) values('Sabor Azulino', 5.0);
+insert into restaurante(nome, taxa_frete) values('Comida Caseira', 2.0);
