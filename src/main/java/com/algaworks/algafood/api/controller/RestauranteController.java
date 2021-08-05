0ac038eecd4a514380157d9713bc70a4c12d3df0
Rodrigo <rodrigoassis1997@gmail.com>
2021-08-04 21:42:37 -0300

    Finalizando a atualização parcial com a API de Reflections do Spring

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +22,7 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -73,5 +78,45 @@ public class RestauranteController {
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		} 
+	}
+	
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+		Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+		
+		if(restauranteAtual == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		merge(campos, restauranteAtual);
+		
+		return atualizar(restauranteId, restauranteAtual);
+	}
+	
+	public void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+		
+		//ObjectMapper é responsável pela serialização e desserialização dos objetos
+		//nesse caso, será  utilizado para atribuir os tipos corretos para os atributos da classe
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		//cria um objeto com os dados de origem usando os tipos corretos (valores convertidos)
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+		
+		//laço necessário para saber quais propriedades o consumidor da API passou
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			//Field do pacote java.lang.reflection e ReflectionUtils do pacote org.springframework.util
+			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+			
+			//altera propriedade privada para ser acessada (foi declarado como private na construção da classe Restaurante)
+			field.setAccessible(true);
+			
+			//necessário para atualizar somente os valores passados pelo consumidor da API
+			//getField busca o valor da propriedade dentro da instância de restauranteOrigem
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			
+			
+			//atualiza o valor
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+		});
 	}
 }
