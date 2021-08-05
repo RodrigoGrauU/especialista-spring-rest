d14941671b85595a0fa3472d0ebdfbe40a37fb38
Rodrigo <rodrigoassis1997@gmail.com>
2021-08-05 20:32:36 -0300

    Implementando consultas JPQL em repositórios

--- src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -7,12 +7,13 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
 	@PersistenceContext
@@ -24,6 +25,13 @@ public class CozinhaRepositoryImpl implements CozinhaRepository {
 		 * Utilizada JPQL para a consulta
 		 */
 		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+	}
+	
+	@Override
+	public List<Cozinha> consultaPorNome(String nome) {
+		return manager.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+				.setParameter("nome", "%" + nome + "%")
+				.getResultList();
 	}
 	
 	/*
