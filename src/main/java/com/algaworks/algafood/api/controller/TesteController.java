f671107422f00bc95397090ca1ba07510e8b2d2f
Rodrigo <rodrigoassis1997@gmail.com>
2021-08-23 20:08:46 -0300

    Injetando o próprio repositório na implementação customizada e a anotação @Lazy

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,8 +1,5 @@
 package com.algaworks.algafood.api.controller;
 
-import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpec.comFreteGratis;
-import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpec.comNomeSemelhante;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
@@ -82,7 +79,6 @@ public class TesteController {
 	
 	@GetMapping("restaurantes/com-frete-gratis")
 	public List<Restaurante> restaurantesComFreteGratis(String nome) {
-		//utilização de métodos estáticos da classe RestauranteSpec
-		return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
+		return restauranteRepository.findComFreteGratis(nome);
 	}
 }
