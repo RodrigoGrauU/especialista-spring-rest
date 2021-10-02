48f0d8171887a340074367302ee4f2985efefd33
Rodrigo <rodrigoassis1997@gmail.com>
2021-10-01 21:21:10 -0300

    8.24. Lançando exception de desserialização na atualização parcial (PATCH)

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,9 +4,14 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +28,7 @@ import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
@@ -71,38 +77,47 @@ public class RestauranteController {
 	}
 	
 	@PatchMapping("/{restauranteId}")
-	public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+	public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos,
+			HttpServletRequest request) {
 		Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 		
-		merge(campos, restauranteAtual);
+		merge(campos, restauranteAtual, request);
 		
 		return atualizar(restauranteId, restauranteAtual);
 	}
 	
-	public void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+	public void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
+		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
+		try {
+			//ObjectMapper é responsável pela serialização e desserialização dos objetos
+			//nesse caso, será  utilizado para atribuir os tipos corretos para os atributos da classe
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+			
+			//cria um objeto com os dados de origem usando os tipos corretos (valores convertidos)
+			Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+			
+			//laço necessário para saber quais propriedades o consumidor da API passou
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+				//Field do pacote java.lang.reflection e ReflectionUtils do pacote org.springframework.util
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+				
+				//altera propriedade privada para ser acessada (foi declarado como private na construção da classe Restaurante)
+				field.setAccessible(true);
+				
+				//necessário para atualizar somente os valores passados pelo consumidor da API
+				//getField busca o valor da propriedade dentro da instância de restauranteOrigem
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+				
+				
+				//atualiza o valor
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			});
+		} catch (IllegalArgumentException e) {
+			Throwable rootCause = ExceptionUtils.getRootCause(e);
+			throw new HttpMessageNotReadableException(e.getMessage(),rootCause,serverHttpRequest);
+		}
 		
-		//ObjectMapper é responsável pela serialização e desserialização dos objetos
-		//nesse caso, será  utilizado para atribuir os tipos corretos para os atributos da classe
-		ObjectMapper objectMapper = new ObjectMapper();
-		
-		//cria um objeto com os dados de origem usando os tipos corretos (valores convertidos)
-		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
-		
-		//laço necessário para saber quais propriedades o consumidor da API passou
-		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			//Field do pacote java.lang.reflection e ReflectionUtils do pacote org.springframework.util
-			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-			
-			//altera propriedade privada para ser acessada (foi declarado como private na construção da classe Restaurante)
-			field.setAccessible(true);
-			
-			//necessário para atualizar somente os valores passados pelo consumidor da API
-			//getField busca o valor da propriedade dentro da instância de restauranteOrigem
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-			
-			
-			//atualiza o valor
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
-		});
 	}
 }
