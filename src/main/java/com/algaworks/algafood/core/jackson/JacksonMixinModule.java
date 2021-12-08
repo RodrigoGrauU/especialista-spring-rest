85512f0b0e0ef9ef8b05e43f11e238a39db516ff
Rodrigo <rodrigoassis1997@gmail.com>
2021-12-07 22:00:27 -0300

    11.12. Refatorando e criando um assembler de DTO

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -2,8 +2,6 @@ package com.algaworks.algafood.core.jackson;
 
 import org.springframework.stereotype.Component;
 
-import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
-import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 @Component
@@ -12,6 +10,6 @@ public class JacksonMixinModule extends SimpleModule {
 	private static final long serialVersionUID = 1L;
 
 	public JacksonMixinModule() {
-		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+//		setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
 	}
 }
