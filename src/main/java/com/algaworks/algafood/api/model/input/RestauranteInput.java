d77d601a8edcfffa7c8f8147ab131bb7bd207a71
Rodrigo <rodrigoassis1997@gmail.com>
2021-12-08 21:20:49 -0300

    11.18. Revisando e ajustando as mensagens de validação com o uso de DTOs

--- src/main/java/com/algaworks/algafood/api/model/input/RestauranteInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/RestauranteInput.java
@@ -4,9 +4,8 @@ import java.math.BigDecimal;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
-
-import com.sun.istack.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
