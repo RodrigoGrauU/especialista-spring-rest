28c08a7c6e867ee0f54b6e55e033fe4a2c360bb9
Rodrigo <rodrigoassis1997@gmail.com>
2022-01-01 11:56:19 -0300

    14.9. Integrando o serviço de catálogo de fotos com o serviço de armazenagem

--- src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.service;
 
 import java.io.InputStream;
+import java.util.UUID;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +10,11 @@ public interface FotoStorageService {
 	
 	void armazenar(NovaFoto novaFoto);
 
+	//realiza implementação na interface mesmo
+	default String gerarNomeArquivo(String nomeOriginal) {
+		return UUID.randomUUID().toString() + "_" + nomeOriginal;
+	}
+	
 	@Builder
 	@Getter
 	class NovaFoto {
