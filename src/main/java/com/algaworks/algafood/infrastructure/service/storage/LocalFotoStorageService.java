c098f7b396dbe2489281e556c28313451e0cbe1c
Rodrigo <rodrigoassis1997@gmail.com>
2022-01-01 15:57:23 -0300

    14.11. Desafio: Implementando recuperação de foto no serviço de armazenagem

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.infrastructure.service.storage;
 
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -43,5 +44,16 @@ public class LocalFotoStorageService implements FotoStorageService {
 		}
 		
 	}
+
+	@Override
+	public InputStream recuperar(String nomeArquivo) {
+	    try {
+	        Path arquivoPath = getArquivoPath(nomeArquivo);
+
+	        return Files.newInputStream(arquivoPath);
+	    } catch (Exception e) {
+	        throw new StorageException("Não foi possível recuperar arquivo.", e);
+	    }
+	}       
 
 }
