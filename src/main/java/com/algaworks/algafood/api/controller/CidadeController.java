fff9fb4650930db7ece14f4336af74610c5db18e
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-02-22 21:27:42 -0300

    19.10. Adicionando hypermedia na representação de recursos de coleção

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -54,9 +55,25 @@ public class CidadeController implements CidadeControllerOpenApi {
 	
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
-	public List<CidadeModel> listar() {
+	public CollectionModel<CidadeModel> listar() {
 		List<Cidade> cidades = cidadeRepository.findAll();
-		return cidadeModelAssembler.toCollectionModel(cidades);
+		List<CidadeModel> cidadesModel = cidadeModelAssembler.toCollectionModel(cidades);
+		
+		cidadesModel.forEach(cidadeModel -> {
+			Link link = linkTo(methodOn(CidadeController.class).buscar(cidadeModel.getId())).withSelfRel();
+			cidadeModel.add(link);
+			
+			cidadeModel.add(linkTo(methodOn(CidadeController.class).listar()).withRel("cidades"));
+			
+			cidadeModel.getEstado().add(linkTo(methodOn(EstadoController.class)
+					.buscar(cidadeModel.getEstado().getId()))
+					.withSelfRel());
+		});
+		
+		CollectionModel<CidadeModel> cidadesCollectionModel = new CollectionModel<>(cidadesModel);
+		cidadesCollectionModel.add(linkTo(CidadeController.class).withSelfRel());
+		
+		return cidadesCollectionModel;
 	}
 	
 	@GetMapping("/{cidadeId}")
