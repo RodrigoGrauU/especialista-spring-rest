ef90cb8b817e6125c736ab9cd256fc7c489bc5eb
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-03-01 17:27:43 -0300

    19.26. Desafio: adicionando template variable de projeção de restaurantes

--- src/main/java/com/algaworks/algafood/api/AlgaLinks.java
+++ src/main/java/com/algaworks/algafood/api/AlgaLinks.java
@@ -32,16 +32,20 @@ public class AlgaLinks {
 			new TemplateVariable("size", VariableType.REQUEST_PARAM),
 			new TemplateVariable("sort", VariableType.REQUEST_PARAM));
 	
-	public Link linkToPedidos() {
-    	TemplateVariables filtroVariables = new TemplateVariables(
-    			new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
-    			new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
-    			new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
-    			new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));
-    	
-    	String pedidoUrl = linkTo(PedidoController.class).toUri().toString();
-    	
-    	return new Link(UriTemplate.of(pedidoUrl, PAGINACAO_VARIABLES.concat(filtroVariables)), "pedidos");
+	public static final TemplateVariables PROJECAO_VARIABLES = new TemplateVariables(
+			new TemplateVariable("projecao", VariableType.REQUEST_PARAM));
+	
+	public Link linkToPedidos(String rel) {
+		TemplateVariables filtroVariables = new TemplateVariables(
+	            new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
+	            new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+	            new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+	            new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));
+	    
+	    String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
+	    
+	    return new Link(UriTemplate.of(pedidosUrl, 
+	            PAGINACAO_VARIABLES.concat(filtroVariables)), rel);
 	}
 	
 	public Link linkToRestaurante(Long restauranteId, String rel) {
@@ -165,7 +169,9 @@ public class AlgaLinks {
 	}
 	
 	public Link linkToRestaurantes(String rel) {
-	    return linkTo(RestauranteController.class).withRel(rel);
+		String restaurantesUrl = linkTo(RestauranteController.class).toUri().toString();
+	    
+	    return new Link(UriTemplate.of(restaurantesUrl, PROJECAO_VARIABLES), rel);
 	}
 
 	public Link linkToRestaurantes() {
