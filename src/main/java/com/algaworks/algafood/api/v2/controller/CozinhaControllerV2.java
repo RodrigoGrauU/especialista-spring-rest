f1ef6b0267c683bbec43c1c687c3fbad524e291b
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-03-31 20:30:19 -0300

    20.15. Desafio: adicionando os recursos de cozinhas na v2 da API

---
+++ src/main/java/com/algaworks/algafood/api/v2/controller/CozinhaControllerV2.java
@@ -0,0 +1,86 @@
+package com.algaworks.algafood.api.v2.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.api.v2.assembler.CozinhaInputDisassemblerV2;
+import com.algaworks.algafood.api.v2.assembler.CozinhaModelAssemblerV2;
+import com.algaworks.algafood.api.v2.model.CozinhaModelV2;
+import com.algaworks.algafood.api.v2.model.input.CozinhaInputV2;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+
+@RestController
+@RequestMapping(value = "/v2/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
+public class CozinhaControllerV2 {
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
+
+	@Autowired
+	private CozinhaModelAssemblerV2 cozinhaModelAssembler;
+	
+	@Autowired
+	private CozinhaInputDisassemblerV2 cozinhaInputDisassembler;
+	
+	@Autowired
+	private PagedResourcesAssembler<Cozinha> pagedResourcesAssembler;
+	
+	@GetMapping
+	public PagedModel<CozinhaModelV2> listar(@PageableDefault(size = 10) Pageable pageable) {
+		Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
+		
+		PagedModel<CozinhaModelV2> cozinhasPagedModel = pagedResourcesAssembler
+				.toModel(cozinhasPage, cozinhaModelAssembler);
+		
+		return cozinhasPagedModel;
+	}
+	
+	@GetMapping(value = "/{cozinhaId}")
+	public CozinhaModelV2 buscar(@PathVariable Long cozinhaId) {
+		return cozinhaModelAssembler.toModel(cadastroCozinha.buscarOuFalhar(cozinhaId));
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED) //retorna o status 201
+	public CozinhaModelV2 adicionar(@RequestBody @Valid CozinhaInputV2 cozinhaInput) {
+		Cozinha cozinha = cozinhaInputDisassembler.toDomainObject(cozinhaInput);
+		return cozinhaModelAssembler.toModel(cadastroCozinha.salvar(cozinha));
+	}
+	
+	@PutMapping(value = "/{cozinhaId}")
+	public CozinhaModelV2 atualizar(@RequestBody @Valid CozinhaInputV2 cozinhaInput, @PathVariable Long cozinhaId) {
+		Cozinha cozinhaAtual = cadastroCozinha.buscarOuFalhar(cozinhaId);
+		
+		cozinhaInputDisassembler.copyToDomainObject(cozinhaInput, cozinhaAtual);
+		Cozinha cozinhaSalva = cadastroCozinha.salvar(cozinhaAtual);
+		return cozinhaModelAssembler.toModel(cozinhaSalva);
+	}
+	
+	@DeleteMapping(value = "/{cozinhaId}")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long cozinhaId) {
+		cadastroCozinha.excluir(cozinhaId);
+	}
+}
---
+++ src/main/java/com/algaworks/algafood/api/v2/model/CozinhaModelV2.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.api.v2.model;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+//import com.algaworks.algafood.api.model.view.RestauranteView;
+//import com.fasterxml.jackson.annotation.JsonView;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Relation(collectionRelation = "cozinhas")
+@Setter
+@Getter
+public class CozinhaModelV2 extends RepresentationModel<CozinhaModelV2> {
+	
+	@ApiModelProperty(example = "1")
+	private Long idCozinha;
+	
+	@ApiModelProperty(example = "Brasileira")
+	private String nomeCozinha;
+}
