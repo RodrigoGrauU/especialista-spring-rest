3d7134f19ba9a0df8b68a0e8c9405220dcfc51d7
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-04-28 19:40:15 -0300

    23.19. Carregando as permissões concedidas na autenticação

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -117,7 +117,7 @@ insert into usuario (id, nome, email, senha, data_cadastro) values
 (4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '$2a$12$j6EPixzWx5u2mKs1Bi6WKus.v9GyYGobmTxHAVY/FlTguP8J04SMW', utc_timestamp),
 (6,	'Pedro Jorge', 'graugames@gmail.com', '$2a$12$j6EPixzWx5u2mKs1Bi6WKus.v9GyYGobmTxHAVY/FlTguP8J04SMW', utc_timestamp);
 
-insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
+insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2), (3, 3), (4, 4);
 
 insert into usuario (id, nome, email, senha, data_cadastro) values
 (5, 'Manoel Lima', 'manoel.loja@gmail.com', '$2a$12$j6EPixzWx5u2mKs1Bi6WKus.v9GyYGobmTxHAVY/FlTguP8J04SMW', utc_timestamp);
