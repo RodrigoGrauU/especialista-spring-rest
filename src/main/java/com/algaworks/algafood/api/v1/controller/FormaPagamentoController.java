3810508cd0c8dd84e30d42a6192a2fc3922ee544
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-05-04 07:24:25 -0300

    23.32. Desafio: restringindo acessos aos endpoints de formas de pagamentos

--- src/main/java/com/algaworks/algafood/api/v1/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/FormaPagamentoController.java
@@ -27,6 +27,7 @@ import com.algaworks.algafood.api.v1.assembler.FormaPagamentoModelAssembler;
 import com.algaworks.algafood.api.v1.model.FormaPagamentoModel;
 import com.algaworks.algafood.api.v1.model.input.FormaPagamentoInput;
 import com.algaworks.algafood.api.v1.openapi.controller.FormaPagamentoControllerOpenApi;
+import com.algaworks.algafood.core.security.CheckSecurity;
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
 import com.algaworks.algafood.domain.service.CadastroFormaPagamentoService;
@@ -47,6 +48,7 @@ public class FormaPagamentoController implements FormaPagamentoControllerOpenApi
 	@Autowired
 	FormaPagamentoInputDesassembler formaPagamentoInputDesassembler;
 	
+	@CheckSecurity.FormasPagamento.PodeConsultar
 	@GetMapping
 	public ResponseEntity<CollectionModel<FormaPagamentoModel>> listar(ServletWebRequest request) {
 		ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
@@ -76,6 +78,7 @@ public class FormaPagamentoController implements FormaPagamentoControllerOpenApi
 				
 	}
 	
+	@CheckSecurity.FormasPagamento.PodeEditar
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public FormaPagamentoModel adicionar(@RequestBody @Valid FormaPagamentoInput formaPagamentoInput) {
@@ -84,6 +87,7 @@ public class FormaPagamentoController implements FormaPagamentoControllerOpenApi
 		return formaPagamentoModelAssembler.toModel(formaPagamento);
 	}
 	
+	@CheckSecurity.FormasPagamento.PodeEditar
 	@PutMapping("/{formaPagamentoId}")
 	public FormaPagamentoModel atualizar(@PathVariable Long formaPagamentoId, @RequestBody @Valid FormaPagamentoInput formaPagamentoInput) {
 		FormaPagamento formaPagamento = service.buscarOuFalhar(formaPagamentoId);
@@ -92,12 +96,14 @@ public class FormaPagamentoController implements FormaPagamentoControllerOpenApi
 		return formaPagamentoModelAssembler.toModel(formaPagamento);
 	}
 	
+	@CheckSecurity.FormasPagamento.PodeEditar
 	@DeleteMapping("/{formaPagamentoId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long formaPagamentoId) {
 		service.excluir(formaPagamentoId);
 	}
 	
+	@CheckSecurity.FormasPagamento.PodeConsultar
 	@GetMapping("/{formaPagamentoId}")
 	public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId, ServletWebRequest request) {
 		ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
--- src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -72,4 +72,18 @@ public @interface CheckSecurity {
 		public @interface PodeGerenciarPedidos {}
 		
 	}
+	
+	public @interface FormasPagamento {
+
+	    @PreAuthorize("hasAuthority('SCOPE_WRITE') and hasAuthority('EDITAR_FORMAS_PAGAMENTO')")
+	    @Retention(RetentionPolicy.RUNTIME)
+	    @Target(ElementType.METHOD)
+	    public @interface PodeEditar { }
+
+	    @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
+	    @Retention(RetentionPolicy.RUNTIME)
+	    @Target(ElementType.METHOD)
+	    public @interface PodeConsultar { }
+	    
+	}
 }
