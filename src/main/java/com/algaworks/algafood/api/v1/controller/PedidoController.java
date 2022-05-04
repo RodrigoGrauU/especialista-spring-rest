37c55b7a795152a285ba199dd7fc5c2bfdd476e5
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-05-04 07:19:39 -0300

    23.31. Desafio: restringindo acessos aos endpoints de transição de status de pedidos

--- src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
@@ -109,6 +109,7 @@ public class PedidoController implements PedidoControllerOpenApi {
         return pedidoModelAssembler.toModel(pedido);
     }
     
+    @CheckSecurity.Pedidos.PodeCriar
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PedidoModel adicionar(@Valid @RequestBody PedidoInput pedidoInput) {
