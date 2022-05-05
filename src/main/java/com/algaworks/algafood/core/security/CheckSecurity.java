9466440c92a7f485a86af7f9f032d9cbc6c0edbe
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-05-04 21:10:16 -0300

    23.38. Corrigindo lógica de restrição de acessos para Client Credentials Flow

--- src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -47,14 +47,14 @@ public @interface CheckSecurity {
 			
 		@PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
 		@PostAuthorize("hasAuthority('CONSULTAR_PEDIDOS') or " 	//post executa apos a chamada do metodo
-				+ "@algaSecurity.getUsuarioId() == returnObject.cliente.id or "
+				+ "@algaSecurity.usuarioAutenticadoIgual(returnObject.cliente.id) or "
 				+ "@algaSecurity.gerenciaRestaurante(returnObject.restaurante.id)") //returnObject referencia o objeto de retorno do metodo anotado
 		@Retention(RetentionPolicy.RUNTIME)
 		@Target(ElementType.METHOD)
 		public @interface PodeBuscar { }
 	
 		@PreAuthorize("hasAuthority('SCOPE_READ') and (hasAuthority('CONSULTAR_PEDIDOS') or " 
-				+ "@algaSecurity.getUsuarioId() == #filtro.clienteId or"
+				+ "@algaSecurity.usuarioAutenticadoIgual(#filtro.clienteId) or"
 				+ "@algaSecurity.gerenciaRestaurante(#filtro.restauranteId))")
 		@Retention(RetentionPolicy.RUNTIME)
 		@Target(ElementType.METHOD)
@@ -118,13 +118,13 @@ public @interface CheckSecurity {
 	public @interface UsuariosGruposPermissoes {
 
 	    @PreAuthorize("hasAuthority('SCOPE_WRITE') and "
-	            + "@algaSecurity.getUsuarioId() == #usuarioId")
+	            + "@algaSecurity.usuarioAutenticadoIgual(#usuarioId)")
 	    @Retention(RetentionPolicy.RUNTIME)
 	    @Target(ElementType.METHOD)
 	    public @interface PodeAlterarPropriaSenha { }
 	    
 	    @PreAuthorize("hasAuthority('SCOPE_WRITE') and (hasAuthority('EDITAR_USUARIOS_GRUPOS_PERMISSOES') or "
-	            + "@algaSecurity.getUsuarioId() == #usuarioId)")
+	            + "@algaSecurity.usuarioAutenticadoIgual(#usuarioId))")
 	    @Retention(RetentionPolicy.RUNTIME)
 	    @Target(ElementType.METHOD)
 	    public @interface PodeAlterarUsuario { }
