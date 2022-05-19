b2e834c03bbf8acfb6873df3346430b32d5edb2c
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-05-19 13:36:03 -0300

    25.3. Dependência de JavaMailSender não satisfeita: melhorando o uso da herança

--- src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -6,13 +6,9 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import com.algaworks.algafood.core.email.EmailProperties;
 import com.algaworks.algafood.domain.service.EnvioEmailService;
-
-import freemarker.template.Configuration;
-import freemarker.template.Template;
 
 public class SmtpEnvioEmailService implements EnvioEmailService {
 
@@ -23,7 +19,7 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	private JavaMailSender mailSender;
 	
 	@Autowired
-	private Configuration freeMarkerConfig;
+	private ProcessadorEmailService processadorEmailService;
 	
 	@Override
 	public void enviar(Mensagem mensagem) {
@@ -37,7 +33,8 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	}
 	
 	protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException {
-	    String corpo = processarTemplate(mensagem);
+//	    String corpo = processarTemplate(mensagem);
+		String corpo = processadorEmailService.processarTemplate(mensagem);
 	    
 	    MimeMessage mimeMessage = mailSender.createMimeMessage();
 	    
@@ -49,17 +46,5 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
 	    
 	    return mimeMessage;
 	}
-
-	
-	protected String processarTemplate(Mensagem mensagem) {
-		try {
-		Template template =	freeMarkerConfig.getTemplate(mensagem.getCorpo());
-			return FreeMarkerTemplateUtils.processTemplateIntoString(template, mensagem.getVariaveis());
-			
-		} catch (Exception e) {
-			throw new EmailException("Não foi possível montar o template do e-mail", e);
-		}
-	}
-
 
 }
