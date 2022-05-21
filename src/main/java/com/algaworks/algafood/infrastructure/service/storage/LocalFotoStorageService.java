29ecac207ac0ffcb6acb697d9c9a4b6118e8a537
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-05-20 23:06:22 -0300

    aulas passadas: 14.18 a 14.26 - Amazon Bucket S3 - adição de objetos

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalFotoStorageService.java
@@ -3,35 +3,44 @@ package com.algaworks.algafood.infrastructure.service.storage;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.FileCopyUtils;
 
+import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 
-@Service
 public class LocalFotoStorageService implements FotoStorageService {
 
-	@Value("${algafood.storage.local.diretorio-fotos}")
-	private Path diretorioFotos;
+	@Autowired
+	private StorageProperties storageProperties;
+	
+	@Override
+	public FotoRecuperada recuperar(String nomeArquivo) {
+		try {
+			Path arquivoPath = getArquivoPath(nomeArquivo);
+
+			FotoRecuperada fotoRecuperada = FotoRecuperada.builder()
+					.inputStream(Files.newInputStream(arquivoPath))
+					.build();
+			
+			return fotoRecuperada;
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível recuperar arquivo.", e);
+		}
+	}
 	
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
 		try {
 			Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
 			
-			FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
-			
+			FileCopyUtils.copy(novaFoto.getInputStream(), 
+					Files.newOutputStream(arquivoPath));
 		} catch (Exception e) {
-			throw new StorageException("Não foi possível armazenar arquivo", e);
+			throw new StorageException("Não foi possível armazenar arquivo.", e);
 		}
-		
 	}
 	
-	private Path getArquivoPath(String nomeArquivo) {
-		return diretorioFotos.resolve(Path.of(nomeArquivo));
-	}
-
 	@Override
 	public void remover(String nomeArquivo) {
 		try {
@@ -39,24 +48,13 @@ public class LocalFotoStorageService implements FotoStorageService {
 			
 			Files.deleteIfExists(arquivoPath);
 		} catch (Exception e) {
-			throw new StorageException("Não foi possível excluir arquivo", e);
+			throw new StorageException("Não foi possível excluir arquivo.", e);
 		}
-		
 	}
 
-	@Override
-	public FotoRecuperada recuperar(String nomeArquivo) {
-	    try {
-	        Path arquivoPath = getArquivoPath(nomeArquivo);
-
-	        FotoRecuperada fotoRecuperada = FotoRecuperada.builder()
-					.inputStream(Files.newInputStream(arquivoPath))
-					.build();
-	        
-	        return fotoRecuperada;
-	    } catch (Exception e) {
-	        throw new StorageException("Não foi possível recuperar arquivo.", e);
-	    }
-	}       
+	private Path getArquivoPath(String nomeArquivo) {
+		return storageProperties.getLocal().getDiretorioFotos()
+				.resolve(Path.of(nomeArquivo));
+	}
 
 }
